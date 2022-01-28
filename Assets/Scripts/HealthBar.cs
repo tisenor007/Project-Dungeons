d16cde19761b4e279142bbae2462b708dddf1141
Jared Trenholm <Jared.Trenholm12@gmail.com>
2022-01-28 09:30:11 -0400

    [Fixed] Spacing

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,14 +7,17 @@ public class HealthBar : MonoBehaviour
     [SerializeField] private PlayerStats playerStats;
     [SerializeField] private GameObject healthBar;
     [SerializeField] private Text healthText;
+
     private void Awake()
     {
         originalScale = healthBar.transform.localScale;
     }
+
     void Update()
     {
         UpdateHealth();
     }
+
     private void UpdateHealth()
     {
         healthBar.transform.localScale = new Vector3(originalScale.x * playerStats.GetHealthDividedMaxHealth(), originalScale.y, originalScale.z);
