c5c6be8e88925bdfc4d6f53b5ca1ad09c7ca1155
tisenor007 <77278112+tisenor007@users.noreply.github.com>
2022-02-17 20:45:35 -0400

    [Added] enemies in generation, bigger gen structs, gen structs % chances

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -11,6 +11,8 @@ public class DungeonGenerator : MonoBehaviour
         Length
     }
     public int structureSpacing = 10;
+    public int roomChance = 80;
+    public int hallwayChance = 10;
     [HideInInspector] public List<GameObject> structures = new List<GameObject>();
     [SerializeField] private GameObject[] roomVariations;
     [SerializeField] private GameObject[] hallwayVariations;
@@ -50,45 +52,37 @@ public class DungeonGenerator : MonoBehaviour
     //tries to add room to dungeon
     private void GenerateNewRoom()
     {
-        nextStructureType = (StructureType)Random.Range(0, (int)StructureType.Length);
+        nextStructureType = (StructureType)ChooseNumbByChance((int)StructureType.Room, (int)StructureType.Hallway, roomChance, hallwayChance);
         roomVariation = Random.Range(0, roomVariations.Length);
         hallwayVariation = Random.Range(0, hallwayVariations.Length);
+        ChooseNextRoomDirection();
 
-        if (structures.Count <= 0) 
+        if (nextStructureType == StructureType.Room)
         {
-            if (nextStructureType == StructureType.Room){InstantiateStructure(roomVariations, roomVariation, Vector3.zero, Vector3.zero);}
-            else if (nextStructureType == StructureType.Hallway) { InstantiateStructure(hallwayVariations, hallwayVariation, Vector3.zero, Vector3.zero); }
+            InstantiateStructure(roomVariations, roomVariation, nextStructureLoc, nextStructureRot);
         }
-       
-        if (structures.Count > 0) 
+        else if (nextStructureType == StructureType.Hallway)
         {
-            if (nextStructureType == StructureType.Room)
-            {
-                ChooseNextRoomDirection();
-                InstantiateStructure(roomVariations, roomVariation, nextStructureLoc, nextStructureRot);
-            }
-            else if (nextStructureType == StructureType.Hallway)
-            {
-                ChooseNextRoomDirection();
-                InstantiateStructure(hallwayVariations, hallwayVariation, nextStructureLoc, nextStructureRot);
-            }
+            InstantiateStructure(hallwayVariations, hallwayVariation, nextStructureLoc, nextStructureRot);
         }
     }
 
     //instantiates & stores room
     private void InstantiateStructure(GameObject[] structureVariations, int structureVariation, Vector3 instantiateLoc, Vector3 instantiateRot)
     {
-        if (!IsStructureHere(instantiateLoc))
-        {
-            structures.Add(Instantiate(structureVariations[structureVariation], instantiateLoc, Quaternion.Euler(instantiateRot)));
-            currentStructureType = (StructureType)nextStructureType;
-            structureVariations[structureVariation].GetComponentInChildren<StructureBehavior>().thisStructureType = currentStructureType;
-        }
+        if (structureVariations.Length <= 0) { return; }
+        if (IsStructureHere(instantiateLoc)) { return; }
+        structures.Add(Instantiate(structureVariations[structureVariation], instantiateLoc, Quaternion.Euler(instantiateRot)));
+        currentStructureType = (StructureType)nextStructureType;
+        structureVariations[structureVariation].GetComponentInChildren<StructureBehavior>().thisStructureType = currentStructureType;
+        
     }
 
     //makes random direction that next room / structure can go
     private void ChooseNextRoomDirection()
     {
+        if (structures.Count <= 0) { nextStructureLoc = Vector3.zero; nextStructureRot = Vector3.zero; return; }
+        //restricts directions based off where it is coming from and where it is going.....
         if (currentStructureType == StructureType.Hallway && structures[structures.Count - 1].transform.eulerAngles.y == 0) { nextStructureDirection = Random.Range(0, 2); }
         else if (currentStructureType == StructureType.Hallway && structures[structures.Count - 1].transform.eulerAngles.y == 90) { nextStructureDirection = Random.Range(2, 3); }
         else if (currentStructureType == StructureType.Room) { nextStructureDirection = Random.Range(0, 3); }
@@ -115,6 +109,14 @@ public class DungeonGenerator : MonoBehaviour
             else if (nextStructureType != StructureType.Hallway) { nextStructureRot = new Vector3(0, 0, 0); }
             nextStructureLoc = new Vector3(structures[structures.Count - 1].transform.position.x, structures[structures.Count - 1].transform.position.y, structures[structures.Count - 1].transform.position.z - structureSpacing);
         }
+    }
+
+    private int ChooseNumbByChance(int output1, int output2, int chance1, int chance2)
+    {
+        int chance = Random.Range(0, 100);
+        if (chance < chance1) { return output1; }
+        else if (chance < chance1+chance2) { return output2; }
+        return 0;
     }
 
     //prevents ovelapping
