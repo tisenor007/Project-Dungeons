3ec0e31e2a9feaa1ae4c929d29ff5ce74dd71f44
sir William <williamfr0615@gmail.com>
2022-03-25 06:04:43 -0300

    [Fixed] Weapon

--- Assets/Scripts/Interact System/Items/Equipment/Weapon.cs
+++ Assets/Scripts/Interact System/Items/Equipment/Weapon.cs
@@ -1,52 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public abstract class Weapon : Equipment
 {
-    public GameObject weaponObject;
     public int damage;
     public float attackSpeed;
 
-    public void StartSetStats(PlayerStats playerStats)
-    {
-        // damage
-        playerStats.Damage = damage;
-
-        // attack speed
-        playerStats.AttackSpeed = attackSpeed;
-
-        // item type
-        playerStats.CurrentWeapon = this;
-    }
-
     public override void Equip(GameObject equipment, GameObject interactor, bool inGameEquip)
     {
+        LevelManager lM = GameManager.manager.levelManager;
+
         base.Equip(equipment, interactor, inGameEquip);
 
         PlayerStats playerStats = FindPlayerStats(interactor);
 
-        // discard old weapon
-        // playerStats.DiscardWeapon();
+        //equip weapon
+        playerStats.EquipWeapon(this, inGameEquip);
 
-        // set player stats
-        // damage
-        playerStats.Damage = damage;
-
-        // attack speed
-        playerStats.AttackSpeed = attackSpeed;
-        
-        //equip 
-        playerStats.EquipWeapon(equipment, inGameEquip);
-
-        // item type
-        playerStats.CurrentWeapon = this;
-
-        //display equip feedback
         if (inGameEquip == false) { return; }
-        GameManager.manager.levelManager.CreatePopUp($"Equipped {this.nameOfItem}:{damage} ATK", interactor.transform.position, Color.white);
-        Debug.LogWarning($"Set {playerStats.gameObject.name}, damage = {damage}, speed = {attackSpeed}, type = {playerStats.CurrentWeapon}.");
+        //display equip feedback ?
+
+        lM.CreatePopUp($"<=Equiped=-", interactor.transform.position, Color.white);
+        DisplayFeedback(lM);
     }
 
+    public void DisplayFeedback(LevelManager lM)
+    { 
+        UIManager uM = GameManager.manager.uiManager;
+
+        lM.WeaponDisplayTimer = 3;
+        //uM.weaponInfo.transform.GetChild(0).GetComponentInChildren<Image>().sprite = this.sprite;
+        uM.weaponInfo.transform.GetChild(0).GetComponentInChildren<Text>().text = $"{nameOfItem}\nDMG:{damage}|SPD:{attackSpeed}";
+        
+        lM.JumpCanvasAlphaTo(10, uM.weaponInfo);
+    }
 }
