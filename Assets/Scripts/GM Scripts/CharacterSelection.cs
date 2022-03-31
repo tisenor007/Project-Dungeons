3d79179763ec7f15c26672fa98131ebaf716a36f
tisenor007 <77278112+tisenor007@users.noreply.github.com>
2022-03-30 23:27:32 -0300

    [Updated] UI Overhaul

--- Assets/Scripts/GM Scripts/CharacterSelection.cs
+++ Assets/Scripts/GM Scripts/CharacterSelection.cs
@@ -6,6 +6,7 @@ public class CharacterSelection : MonoBehaviour
     [HideInInspector] public bool isMale = true;
     [SerializeField] private GameObject maleDisplay;
     [SerializeField] private GameObject femaleDisplay;
+    [SerializeField] private GameObject backgroundDisplay;
     [SerializeField] private Text characterNameText;
 
     private enum Character
@@ -53,6 +54,7 @@ public class CharacterSelection : MonoBehaviour
     {
         maleDisplay.SetActive(isMale);
         femaleDisplay.SetActive(!isMale);
+        backgroundDisplay.SetActive(true);
     }
 
 
@@ -60,5 +62,6 @@ public class CharacterSelection : MonoBehaviour
     {
         maleDisplay.SetActive(false);
         femaleDisplay.SetActive(false);
+        backgroundDisplay.SetActive(false);
     }
 }
